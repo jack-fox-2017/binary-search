e4b21270b0bb8d47e9d984f1e21077e398810c9b
Rahmat H <kickyou44@gmail.com>
2017-07-25 16:14:57 +0700

    binary search selesai

--- binary.js
+++ binary.js
@@ -5,19 +5,51 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  for (var i = 1; i < arr.length; i++) {
+  	for (var j = 0; j < arr.length; j++) {
+  		if (arr[i] < arr[j]) {
+  			var temp = arr[i]
+  			arr[i] = arr[j]
+  			arr[j] = temp
+  		}
+  	}
+  }
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  var ujungKiri = 0
+  var ujungKanan = array.length - 1
+  var mid = Math.ceil((ujungKiri + ujungKanan) / 2)
+
+  while (ujungKiri < mid && mid < ujungKanan) {
+  // console.log(ujungKiri, mid, ujungKanan);
+  	if (search == array[mid]) return mid
+  	else if (search == array[ujungKiri]) return ujungKiri
+  	else if (search == array[ujungKanan]) return ujungKanan
+  	else {
+	  	var kiri = search <= array[mid]
+	  	var kanan = search <= array[ujungKanan]
+  		
+  		if (kiri && kanan) {
+  			ujungKanan = mid
+  		} else {
+  			ujungKiri = mid
+  		}
+
+  		mid = Math.ceil((ujungKiri + ujungKanan) / 2)
+  	}
+  }
+
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
+console.log(binary_search(8, arrayGenapSorted)) 
 console.log(binary_search(10, arrayGenapSorted))
 console.log(binary_search(33, arrayGenapSorted))
 
